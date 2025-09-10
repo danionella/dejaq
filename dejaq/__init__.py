e6d2ea0a954cde2b58cf757a1970dd8a14f45ec3
Benjamin <bjudkewitz@users.noreply.github.com>
2025-09-11 00:11:31 +0200

    logging

--- dejaq/__init__.py
+++ dejaq/__init__.py
@@ -3,5 +3,8 @@
 """
 __docformat__ = 'google'
 
+import logging, sys
+logging.basicConfig(level=logging.INFO,format="%(asctime)s %(levelname)s [%(name)s:%(lineno)d] %(funcName)s: %(message)s", stream=sys.stdout)
+
 from .queues import DejaQueue
 from .parallel import Parallel
