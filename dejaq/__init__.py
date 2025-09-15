7ddfde8f62aa0162a4962e622be598064d864205
Benjamin <bjudkewitz@users.noreply.github.com>
2025-09-15 12:52:59 +0200

    init imports

--- dejaq/__init__.py
+++ dejaq/__init__.py
@@ -6,5 +6,6 @@ __docformat__ = 'google'
 import logging, sys
 logging.basicConfig(level=logging.INFO,format="%(asctime)s %(levelname)s [%(name)s:%(lineno)d] %(funcName)s: %(message)s", stream=sys.stdout)
 
-from .queues import DejaQueue
+from .queues import DejaQueue, PicklableDejaQueue
 from .parallel import Parallel
+from .remote import Actor, RemoteFunc, ActorDecorator
